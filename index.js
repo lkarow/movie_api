a7bc6019815eaae8dd4995739d3e3005284ae475
Lennart <lkarow@online.de>
2022-04-30 21:46:54 +0200

    add request routes

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express'),
   morgan = require('morgan'),
   fs = require('fs'),
-  path = require('path');
+  path = require('path'),
+  bodyParser = require('body-parser'),
+  uuid = require('uuid');
 
 const app = express();
+
+app.use(bodyParser.json());
 
 // create a write stream in log.txt
 const accessLogStream = fs.createWriteStream(path.join(__dirname, 'log.txt'), {
@@ -46,9 +50,66 @@ let topMovies = [
   },
 ];
 
-// GET requests
+let users = [
+  {
+    id: 1,
+    name: 'test name',
+    mail: 'test mail',
+  },
+];
+
+// get all movies
 app.get('/movies', (req, res) => {
   res.json(topMovies);
+});
+
+// get data about a single movie
+app.get('/movies/:movieTitle', (req, res) => {
+  res.send('Successful GET request returning data about SINGLE MOVIE');
+});
+
+// get data about genre
+app.get('/genres/:genreTitle', (req, res) => {
+  res.send('Successful GET request returning data about a GENRE');
+});
+
+// get data about director
+app.get('/directors/:name', (req, res) => {
+  res.send('Successfull GET request returning data about DIRECTOR');
+});
+
+// add new user
+app.post('/users', (req, res) => {
+  let newUser = req.body;
+
+  if (!newUser.name) {
+    const message = 'Missing name in request body';
+    res.status(400).send(message);
+  } else {
+    newUser.id = uuid.v4();
+    users.push(newUser);
+    res.status(201).send(newUser);
+  }
+});
+
+// change user name
+app.put('/users/:id/:newUserName', (req, res) => {
+  res.send('Successfull PUT request CHANGING user name');
+});
+
+// add movie to favorites list of user
+app.put('/users/:id/movies/:movieTitle', (req, res) => {
+  res.send('Successfull PUT request ADDING MOVIE to favorite list');
+});
+
+// delete movie from favorite list of user
+app.delete('/users/:id/movies/:movieTitle', (req, res) => {
+  res.send('Successfull DELETE request REMOVING MOVIE to favorite list');
+});
+
+// delete user
+app.delete('/users/:id/', (req, res) => {
+  res.send('Successful DELETE request REMOVING USER');
 });
 
 app.get('/', (req, res) => {
