6912b2ed16496a976e3ccd28255d5ae3807853d1
Lennart <lkarow@online.de>
2022-05-29 15:11:32 +0200

    fix user-update

--- index.js
+++ index.js
@@ -201,12 +201,13 @@ app.put(
   '/users/:Username',
   passport.authenticate('jwt', { session: false }),
   (req, res) => {
+    let hashedPassword = Users.hashPassword(req.body.Password);
     Users.findOneAndUpdate(
       { Username: req.params.Username },
       {
         $set: {
           Username: req.body.Username,
-          Password: req.body.Password,
+          Password: hashedPassword,
           Email: req.body.Email,
           Birthday: req.body.Birthday,
         },
