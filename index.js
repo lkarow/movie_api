861fe8d1276fdf069063681620eaa9ca32daca31
Lennart <lkarow@online.de>
2022-08-06 16:04:58 +0200

    configure CORS settings and allowed origins

--- index.js
+++ index.js
@@ -26,7 +26,24 @@ const app = express();
 app.use(bodyParser.json());
 
 const cors = require('cors');
-app.use(cors());
+let allowedOrigins = [
+  'https://myflix-501fc4.netlify.app/',
+  'http://localhost:1234',
+];
+
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        // If a specific origin isn’t found on the list of allowed origins
+        let message = `The CORS policy for this application doesn't allow access from origin ${origin}`;
+        return callback(new Error(message), false);
+      }
+      return callback(null, true);
+    },
+  })
+);
 
 require('./auth')(app);
 const passport = require('passport');
